4539fdf89226784d74048190ab37d02a27876c9d
eDonkey <kooltheoutsider@gmail.com>
2022-09-28 00:00:16 -0300

    Update connection.php

--- api/connection.php
+++ api/connection.php
@@ -2,7 +2,7 @@
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 if ($_SERVER['HTTP_HOST'] == "192.168.64.2") {
     $mysqli = new mysqli('localhost', 'root', 'Corrientes818!', 'metal');
-} else {
+} elseif ($_SERVER['HTTP_HOST'] == "suppliers.cordiak.com") {
     $mysqli = new mysqli('localhost', 'metal', 'Merlina97!', 'metal');
 }
 $mysqli->set_charset('utf8mb4');
